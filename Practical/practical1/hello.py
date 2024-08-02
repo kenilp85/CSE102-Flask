520348dc3bfeef4d6f41552b3671bf78c8e76d2d
Kenil Patel <122993935+kenilp85@users.noreply.github.com>
2024-08-02 09:17:58 +0530

    first upload
    This is the first upload with the edited file

--- Practical/practical1/hello.py
+++ Practical/practical1/hello.py
@@ -1,1 +1,23 @@
+from flask import Flask
+from flask import render_template
+from flask import request
 
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/index", methods=['GET', 'POST'])
+def hello1():
+   if request.method == 'POST':
+     Fname = request.form.get('fname')
+     Lname = request.form.get('lname')
+     return render_template('index.html', Fname=Fname, Lname=Lname)
+@app.route("/aboutus")
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route("/contactus")
+def contactus():
+    return render_template('contactus.html')
