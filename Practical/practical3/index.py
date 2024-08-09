bfb2ba296707684ff3bbc52c9b156279c5fad304
kenilp85 <kenilp85@gmail.com>
2024-08-09 08:27:04 +0530

    practical3 initial

---
+++ Practical/practical3/index.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+def hello():
+     return render_template('index.html')
+
+@app.route("/verify",methods=['GET', 'POST'])
+def hello1():
+    if request.method=='POST':
+        fullname=request.form.get('nm')
+        age = request.form.get('age')
+        return render_template('verify.html',fullname=fullname, age=age)
