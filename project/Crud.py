9af52399d4850efc48d27b28b0b7cdb0b225f3ad
kenilp85 <kenilp85@gmail.com>
2024-09-13 08:52:45 +0530

    Project

---
+++ project/Crud.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy import text
+engine = create_engine("sqlite:///student.db", echo=True)
+
+class Crud:
+    def search(id):
+        
+    def select():
+        
+    def update():
+        
+    def delete():
+        
+    def insert():
